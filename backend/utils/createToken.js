e54af88664ba8f0e2a34bc794ffa4a0a531595cd
Aayush Poudel <aayushp2906@gmail.com>
2024-08-16 01:02:08 +0545

    updated createToken to expire the token in 30 days instead of 60 minutes

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "60m",
+    expiresIn: "30d",
   });
 
   // Set JWT as an HTTP-Only Cookie
