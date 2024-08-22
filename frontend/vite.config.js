07a2ef3a71664fe5349dca96bbfda99ef6774760
Aayush Poudel <aayushp2906@gmail.com>
2024-08-22 16:40:47 +0545

    Configured SSL certification

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "localhost.key")),
+      cert: fs.readFileSync(path.resolve(__dirname, "localhost.crt")),
+    },
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api": {
+        target: "https://localhost:5000",
+        changeOrigin: true,
+        secure: false, // Bypass SSL certificate validation
+      },
+      "/uploads": {
+        target: "https://localhost:5000",
+        changeOrigin: true,
+        secure: false, // Bypass SSL certificate validation
+      },
     },
   },
 });
